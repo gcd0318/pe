214626dee21df0bad1ac366de8a615fedbae4c68
gcd0318 <gcd0318@hotmail.com>
2019-12-15 23:25:54 +0800

    118 going

--- l5/pe118.py
+++ l5/pe118.py
@@ -4,3 +4,115 @@ Using all of the digits 1 through 9 and concatenating them freely to form decima
 How many distinct sets containing each of the digits one through nine exactly once contain only prime elements?
 """
 
+import math
+
+def lessPrime(n):
+    res = []
+    l = [0,0]
+    for i in range(2, n+1):
+        l.append(i)
+    m = len(l)//2 + 1
+    i = 2
+    while(i <= m):
+        while (0 != l[i]):
+            t = i * 2
+            while (t < len(l)):
+                l[t] = 0
+                t = t + i
+            i = i + 1
+        i = i + 1
+    for i in l:
+        if(0 != i):
+            res.append(i)
+    return res
+
+def intoPrime(n):
+    half = math.floor(math.sqrt(n))
+    l = [0, 0]
+    for i in range(2, half+1):
+        l.append(i)
+    i = 0
+    while (i < len(l)):
+        t = i
+        if (0 != l[i]):
+            while(t + i < len(l)):
+                t = t + i
+                l[t] = 0
+        i = i + 1
+    i = 0
+    while((0 < len(l)) and ((0 == i) or (0 != n % i))):
+        i = l.pop()
+        if (0 != i):
+            if (0 == n % i):
+                return [i] + intoPrime(n//i)
+    return [n]
+
+def prime_rate(x):
+    d = {}
+    if(1 < x):
+        l = intoPrime(x)
+        for i in l:
+            d[i] = 0
+        for i in l:
+            r = 0
+            t = x
+            while(0 == t % i):
+                r = r + 1
+                t = t // i
+            d[i] = r
+    return d
+
+def top_n_primes(n):
+    primes = []
+    if (0 < n):
+        ins = 2
+        primes = [2]
+        while(len(primes) < n):
+            ins = ins + 1
+            devided = False
+            i = 0
+            while not(devided) and (i < len(primes)):
+                devided = (0 == ins % primes[i])
+                i = i + 1
+            if not devided:
+                primes.append(ins)
+    return primes
+
+def nth_prime(n):
+    res = 0
+    if (0 < n):
+        res = top_n_primes(n)[-1]
+    return res
+
+def mul(l):
+    res = 1
+    for i in l:
+        res = res * i
+    return res
+
+def isPrime(n):
+    l = [0,0]
+    if(2 > n):
+        return None
+    else:
+        for i in range(2, n+1):
+            l.append(i)
+        for i in l:
+            t = i
+            if(0 != l[i]):
+                t = t + i
+                while(t < len(l)):
+                    l[t] = 0
+                    t = t + i
+    return(0 != l[n])
+
+def betweenPrimes(s, e):
+    resl = []
+    for i in lessPrime(e):
+        if i >=s:
+            resl.append(i)
+    return resl
+
+
+ps = lessPrime(98765431)
+print(len(ps))
